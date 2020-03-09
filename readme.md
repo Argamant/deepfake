42de7214d1cfe2c258b59dccf52fa5da884e98fd
Mike Ruggeri <epictreestick@protonmail.com>
2020-03-09 15:31:28 -0500

    more setup

---
+++ readme.md
@@ -0,0 +1,10 @@
+# getting started:
+
+install keras and tensorflow: https://keras.io/#installation  
+
+The keras installation guide will point you to tensorflow.
+
+## Kaggle Competition:  
+https://www.kaggle.com/c/deepfake-detection-challenge  
+
+The data is here: https://www.kaggle.com/c/deepfake-detection-challenge/data
--- transfer_vgg.py
+++ transfer_vgg.py
@@ -1,6 +1,18 @@
 # example of loading the vgg16 model
 from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+
 # load model
 model = VGG16()
 # summarize the model
 model.summary()
+
+
+# load an image from file
+image = load_img('dog.jpg', target_size=(224, 224))
+# convert the image pixels to a numpy array
+image = img_to_array(image)
+# reshape data for the model
+image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+# prepare the image for the VGG model
+image = preprocess_input(image)
