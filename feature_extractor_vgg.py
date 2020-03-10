484ac054ebffafb4f319dea38ea3061d01a80341
Mike Ruggeri <epictreestick@protonmail.com>
2020-03-10 15:53:01 -0500

    added new tutorial code from https://www.pyimagesearch.com/2019/07/15/video-classification-with-keras-and-deep-learning/

--- feature_extractor_vgg.py
+++ feature_extractor_vgg.py
@@ -1,4 +1,6 @@
 # example of using the vgg16 model as a feature extraction model
+
+
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.applications.vgg16 import preprocess_input
@@ -6,12 +8,16 @@ from keras.applications.vgg16 import decode_predictions
 from keras.applications.vgg16 import VGG16
 from keras.models import Model
 from pickle import dump
+
+import pandas as pd
+
 # load an image from file
 image = load_img('big_and_bootiful/frame0.jpg', target_size=(224, 224))
 # convert the image pixels to a numpy array
 image = img_to_array(image)
 # reshape data for the model
 image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+#print(image)
 # prepare the image for the VGG model
 image = preprocess_input(image)
 # load model
@@ -21,6 +27,9 @@ model.layers.pop()
 model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
 # get extracted features
 features = model.predict(image)
-print(features.shape)
+#print(features.shape)
 # save to file
 dump(features, open('bnb.pkl', 'wb'))
+#save to pd dataframe
+feat = pd.DataFrame(features)
+print(feat)
