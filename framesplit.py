c6ee47e7f5c9e5e097371a91cd2aeedef6a8d95e
Mike Ruggeri <epictreestick@protonmail.com>
2020-03-14 13:29:31 -0500

    better framesplitting and sound extraction

--- framesplit.py
+++ framesplit.py
@@ -1,12 +1,22 @@
 #basic method for separating a video into frames
 from os import mkdir
 import cv2
-vidcap = cv2.VideoCapture('big_and_bootiful.mp4')
-mkdir('big_and_bootiful/')
-success,image = vidcap.read()
-count = 0
-while success:
-  cv2.imwrite("big_and_bootiful/frame%d.jpg" % count, image)     # save frame as JPEG file      
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
+from json import load
+from os import system
+from glob import glob
+
+def extract(source_dir, destination, delim='/')
+  file_list = glob(source_dir + '*.mp4')
+
+  labels = open(source_dir + 'metadata.json', "r")
+  data = load(labels)
+  os.mkdir(destination + 'frames' + delim)
+  for vid in file_list:
+      #the file name without the .mp4 file extension
+      key = vid.split(delim)[-1][:-4]
+      system('ffmpeg -i {} -vf \"scale=320:240,fps=1\" \"'.format(vid) + destination + 'frames\\{}_%04d.jpeg\"'.format(key))
+      #system('ffmpeg -i {} \"'.format(vid) + destination + 'audio\\{}.wav\"'.format(key))
+      i+=1
+      
+  labels.close()
+  
