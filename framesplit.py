4c16886c0daf26c205b68ef6e3a02206ecedb62e
Mike Ruggeri <epictreestick@protonmail.com>
2020-03-15 00:45:33 -0500

    cleaning up

--- framesplit.py
+++ framesplit.py
@@ -5,18 +5,30 @@ from json import load
 from os import system
 from glob import glob
 
-def extract(source_dir, destination, delim='/')
+def extract(source_dir, destination, delim='/'):
   file_list = glob(source_dir + '*.mp4')
 
   labels = open(source_dir + 'metadata.json', "r")
   data = load(labels)
-  os.mkdir(destination + 'frames' + delim)
+  #os.mkdir(destination + 'frames' + delim)
+  #key = vid.split(delim)[-1][:-4]
+  #print(data[file_list[0][-1][:-4]])
+
   for vid in file_list:
       #the file name without the .mp4 file extension
-      key = vid.split(delim)[-1][:-4]
-      system('ffmpeg -i {} -vf \"scale=320:240,fps=1\" \"'.format(vid) + destination + 'frames\\{}_%04d.jpeg\"'.format(key))
-      #system('ffmpeg -i {} \"'.format(vid) + destination + 'audio\\{}.wav\"'.format(key))
-      i+=1
+        key = vid.split(delim)[-1][:-4]
+        real_fake = data[vid.split(delim)[-1]]['label']=='REAL'
+        if real_fake:
+            full_dest =  destination + 'real' + delim
+        else:
+            full_dest =  destination + 'fake' + delim
+
+        system('ffmpeg -i {} -vf \"scale=320:240,fps=1\" \"'.format(vid) + full_dest + '{}_%08d.jpeg\"'.format(key))
+      
+      # system('ffmpeg -i {} \"'.format(vid) + destination + 'audio'+delim+'{}.wav\"'.format(key))
+      #i+=1
       
   labels.close()
-  
+
+
+extract('C:\\Users\\rugge\\source\\repos\\deepfake\\deepfake\\data\\dfdc_train_part_02\\dfdc_train_part_2\\', 'G:\\deepfake\\dfdc_train_part_2\\', '\\')
