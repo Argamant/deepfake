b939d925599f18c0b5be0858b555895a27304f23
Mike Ruggeri <epictreestick@protonmail.com>
2020-03-16 20:20:29 -0500

    added feature extractor to preprocessing notebook

--- feature_extractor_vgg.py
+++ feature_extractor_vgg.py
@@ -12,7 +12,7 @@ from pickle import dump
 import pandas as pd
 
 # load an image from file
-image = load_img('C:\\Users\\rugge\\source\\repos\\deepfake\\deepfake\\demo\\test\\real\\uazbhwyysx00000008.jpg', target_size=(224, 224))
+image = load_img('C:\\Users\\rugge\\source\\repos\\deepfake\\deepfake\\demo\\test\\fake\\aydiylfrlh00000005.jpg', target_size=(224, 224))
 # convert the image pixels to a numpy array
 image = img_to_array(image)
 # reshape data for the model
@@ -32,4 +32,4 @@ features = model.predict(image)
 dump(features, open('bnb.pkl', 'wb'))
 #save to pd dataframe
 feat = pd.DataFrame(features)
-print(feat)
+print(features)
